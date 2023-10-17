12335bffc12cdaef020de8be29d727d94c7822cd
Tarun Dadlani <53266806+tarun080698@users.noreply.github.com>
2023-10-17 16:09:51 -0400

    changed: class to className

--- components/loading.js
+++ components/loading.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div class="flex place-items-center m-auto gap-2">
-      <div class="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
-      <div class="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
-      <div class="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
+    <div className="flex place-items-center m-auto gap-2">
+      <div className="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
+      <div className="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
+      <div className="w-3 h-3 rounded-full animate-pulse bg-gray-500"></div>
     </div>
   );
 }

5076240b3d18a1bfa916a996861e5c5a977e7526
Tarun Dadlani <tarun3dadlani@gmail.com>
2024-03-05 21:08:23 -0500

    changes to OenpAI api

--- components/ModelSelection.js
+++ components/ModelSelection.js
@@ -10,7 +10,7 @@ function ModelSelection() {
   const { data: modelOptions, isLoading } = useSWR("models", fetchModels);
 
   const { data: model, mutate: setModel } = useSWR("model", {
-    fallbackData: "text-davinci-003",
+    fallbackData: "gpt-3.5-turbo",
   });
   return (
     <div className="my-2">
@@ -26,7 +26,6 @@ function ModelSelection() {
         onChange={(e) => setModel(e.value)}
         placeholder={model}
         defaultValue={model}
-        clas
       />
     </div>
   );

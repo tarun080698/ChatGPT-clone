f6ac1915c82d7cf537ceee675cb4974fa1bb0ea2
Tarun Dadlani <tarun3dadlani@gmail.com>
2024-07-30 15:18:48 -0400

    fixed the formatting of gpt responses

--- components/Message.js
+++ components/Message.js
@@ -1,16 +1,22 @@
+"use client";
 import React from "react";
+import parse from "html-react-parser";
 
 function Message({ message }) {
   const isChatGPT = message?.user?.name === "ChatGPT";
   return (
-    <div className={`py-5 text-white ${isChatGPT ? "bg-[#434654]" : ""}`}>
-      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-        <img
-          src={message?.user?.avatar}
-          alt="user_profile"
-          className="h-8 w-8 "
-        />
-        <p className="pt-1 text-sm">{message?.text}</p>
+    <div
+      className={`py-5 text-white ${
+        isChatGPT ? "bg-[#434654]" : ""
+      } flex space-x-5 px-10 `}
+    >
+      <img
+        src={message?.user?.avatar}
+        alt="user_profile"
+        className="h-8 w-8 "
+      />
+      <div className="max-w-2xl mx-auto pt-1 text-base">
+        {isChatGPT ? parse(message?.text) : message?.text}
       </div>
     </div>
   );
